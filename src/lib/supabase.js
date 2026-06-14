db8858cb1de1029c95bf16e779a5f74d44fae5ae
idksebastian <sebaslizarazo02@gmail.com>
2026-06-14 00:10:41 -0500

    debug: log variables de entorno en supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,11 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
+console.log('SUPABASE URL:', import.meta.env.VITE_SUPABASE_URL)
+
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY,
   {
     auth: {
-      storage: window.localStorage, 
+      storage: window.localStorage,
       persistSession: true,
     }
   }
